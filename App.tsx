cd75faf8803e04a7d384a6c962f1b7b159fd0cd2
Nathan Vieira <nathan.vsouza@stone.com.br>
2025-07-08 22:51:15 -0300

    feat: add the react navigation library

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@
  *
  * @format
  */
-
+import React from 'react';
+import { Button } from '@react-navigation/elements';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
   Text,
   View,
@@ -13,13 +16,50 @@ import {
   useColorScheme,
 } from 'react-native';
 
+function ScreenOne() {
+  return (
+    <View style={styles.container}>
+      <Text>Screen One</Text>
+      <Button screen="ScreenTwo" params={{}}>
+        Go to Screen Two
+      </Button>
+    </View>
+  );
+}
+
+function ScreenTwo() {
+  return (
+    <View style={styles.container}>
+      <Text>Screen Two</Text>
+      <Button screen="ScreenOne" params={{}}>
+        Go to Screen One
+      </Button>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'ScreenOne',
+  screens: {
+    ScreenOne: ScreenOne,
+    ScreenTwo: ScreenTwo,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+const linking = {
+  enabled: 'auto' as const,
+  prefixes: [],
+};
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Hello World!</Text>
+      <Navigation linking={linking} />
     </View>
   );
 }
